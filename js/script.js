e2b3cbbcb7c6aaf399824217c6ae49b40e6f2d35
Giovanni Capelli <giovacape01@gmail.com>
2022-12-23 09:46:20 +0100

    scaletta in js

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/*
+
+    STEP:
+    1) generare un numero random per l'utente da 1 a 6
+    2) generare un numero random per il computer da 1 a 6
+    3) Confrontare i punteggi
+    3a) SE il numero dell'utente è più ALTO, dire che ha vinto l'utente
+    3b) SE il numero del computer è più alto, dire che ha vinto il computer
+    4) ALTRIMENTI dire che c'è stato pareggio
+  
+
+
+
+
+
+*/
