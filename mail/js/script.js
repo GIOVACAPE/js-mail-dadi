4d9d3049edf25d0b9576821d81531b0ba7597a43
Giovanni Capelli <giovacape01@gmail.com>
2022-12-23 10:33:38 +0100

    Cretata seconda cartella + add step mail

---
+++ mail/js/script.js
@@ -0,0 +1,12 @@
+/*
+
+STEP:
+1) chiedi EMAIL
+2) valida EMAIL
+3) crea una lista
+4) per ogni elemento della lista:
+4a) Controllo se l'elemento attuale è uguale all'email inserita dall'utente
+4aa)SE i due elementi corrispondo, so che l'utente può entrare e lo avviso
+4ab) ALTRIMENTI, l'utente non può entrare e lo avviso
+
+*/
