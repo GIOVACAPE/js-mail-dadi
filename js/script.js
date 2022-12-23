384687810ec62c0b3a8a7385cd172090df0e65aa
Giovanni Capelli <giovacape01@gmail.com>
2022-12-23 10:03:23 +0100

    Completato DADI, manca mail

--- js/script.js
+++ js/script.js
@@ -7,10 +7,27 @@
     3a) SE il numero dell'utente è più ALTO, dire che ha vinto l'utente
     3b) SE il numero del computer è più alto, dire che ha vinto il computer
     4) ALTRIMENTI dire che c'è stato pareggio
-  
-
-
-
-
 
 */
+
+const userNumber = Math.floor(Math.random() * 6 + 1);
+const pcNumber = Math.floor(Math.random() * 6 + 1);
+
+console.log('userNumber' , userNumber, typeof userNumber);
+console.log('pcNumber' , pcNumber, typeof pcNumber);
+
+if (userNumber > pcNumber ){
+    alert('Hai vinto, BRAVO !!');
+    console.log('Utente vince con numero + grande', userNumber);
+    console.log('PC perde perchè ha il numero + piccolo', pcNumber);
+}
+else if (userNumber < pcNumber){
+        alert('Hai perso, mi spiace');
+        console.log('Utente perde perchè ha il numero piccolo', userNumber);
+        console.log('PC vince  perchè ha il numero + grande', pcNumber);
+        
+}
+else {
+    alert('Pareggio');
+    console.log('Utente e PC pareggiano con numero' , userNumber);
+}
